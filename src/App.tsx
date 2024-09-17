2b190a156abfc199d32e1957e8269ed3b9e6acf5
Raaz <21ketanraaz@gmail.com>
2024-09-17 09:26:07 +0530

    Checkout Page Implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
-import BuyCoupons from "./Pages/BuyCoupons/BuyCoupons"; // Import the BuyCoupons component
+import BuyCoupons from "./Pages/BuyCoupons/BuyCoupons";
+import Checkout from "./Pages/Checkout/Checkout";
 import { CartProvider } from "./context/CartContext";
+import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} /> {/* Landing page route */}
-        <Route
-          path="/buy-coupons"
-          element={
-            <CartProvider>
-              <BuyCoupons />
-            </CartProvider>
-          }
-        />{" "}
-        {/* Buy Coupons route */}
-      </Routes>
-    </Router>
+      <Router>
+        <CartProvider>
+          <Routes>
+            {/* Landing page route */}
+            <Route path="/" element={<LandingPage />} />
+
+            {/* Buy Coupons route */}
+            <Route path="/buy-coupons" element={<BuyCoupons />} />
+
+            {/* Protected Checkout route */}
+            <Route
+              path="/checkout"
+              element={
+                <SignedIn>
+                  <Checkout />
+                </SignedIn>
+              }
+            />
+
+            {/* If not signed in, redirect to sign-in */}
+            <Route
+              path="/checkout"
+              element={
+                <SignedOut>
+                  <RedirectToSignIn />
+                </SignedOut>
+              }
+            />
+          </Routes>
+        </CartProvider>
+      </Router>
   );
 }
 
